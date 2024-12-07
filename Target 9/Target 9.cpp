fcb8d46327f7c17886f5c8be6203fd9fcd4c62a8
Zulfat Z. <zulfat060405@gmail.com>
2024-12-07 22:16:57 +0200

    1

--- Target 9/Target 9.cpp
+++ Target 9/Target 9.cpp
@@ -1,4 +1,4 @@
-//Zulfat Zigangirov U234N1241 04/12/2024
+//Zulfat Zigangirov U234N1241 07/12/2024
 //Java Course Changed me...
 
 #include <iostream>
@@ -7,49 +7,73 @@
 
 class Board {
 private:
+    
     int counter = 0;
     int Grid[3][3] = { {9,9,9},{9,9,9},{9,9,9} };
     
 public:
+    //Will change mod to see past and future
+    bool seq = 0;
     int row, col;
     int difficultyVar;
 
-    int entry[2];
+    //int entry[2];
  
-
+    //Methods
     void ShowBoard() const;
-    void changeGrid(int row, int col);
+    void changeGrid(int row, int col ,bool seq);
     void Difficulty();
     bool Finish() const;
-    bool ValidationCheck() const;
+
+
+
+    struct Entry {
+        int row;
+        int col;
+    };
 
 
     //Our Stack
     struct Stack {
     
-        int counter=0;
-        int entry[2];
-    
+        int counter = 0;
+        Entry entry;
 
         Stack* next;
         
-
         Stack() { next = nullptr; }
         Stack (int r,int c,Stack *n){ 
-            entry[0] = r;
-            entry[1] = c;
+            entry.row = r;
+            entry.col = c;
             next = n;
+            counter++;
         }
+        //is it empty?
         bool empty();
+        //delete
         void pop();
 
+        void push(int,int);
+        //find size
+        int size() const{ return counter; }
 
-        void Undo() {
-           
+
+        void Undo(Board& board) {
+          
+            //easter egg with printf
+            printf("\nMethod Undo\n");
+
+
+            board.changeGrid(this->next->entry.row, this->next->entry.col, 1);
+
+            std::cout << "\n";
+
+            board.ShowBoard();
+            counter--;
 
         }
         void Redo() {
-
+            std::cout << "Redo\n";
 
         }
     };
@@ -57,7 +81,7 @@ public:
 
 
 };
-
+//Check for Stack is empty
 bool Board::Stack::empty() {
 
     if (this == nullptr) return 1;
@@ -65,6 +89,8 @@ bool Board::Stack::empty() {
     return 0;
 
 }
+
+
 void Board::Stack::pop() {
     if (empty())return;
     //create new var, remove it from the sequince and delete
@@ -74,11 +100,12 @@ void Board::Stack::pop() {
 
 }
 
+void Board::Stack::push(int row, int col) {
+
+    this->next = new Stack(row,col, this->next); // Create a new node and link it to the previous
+    this->counter++; // Increment the counter
 
 
-bool Board::ValidationCheck() const{
-
-    return 0;
 }
 
 
@@ -100,7 +127,7 @@ void Board::Difficulty() {
     srand(time(0));
 
     for (int k = 0; k < difficultyVar; k++) {
-        int row = rand() % 3, col=rand() % 3;
+        int row = rand() % 3 + 1, col = rand() % 3 + 1;
 
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -115,16 +142,27 @@ void Board::Difficulty() {
 }
 
 
-void Board::changeGrid(int row, int col) {
+void Board::changeGrid(int row, int col, bool seq) {
+    /*
+    * seq==0 func works as it must by default Grid[i][j]+=1;
+    * seq==1 funk works to see the past Grid[i][j]-=1;
+    */
 
+
+    
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             if (i == row || j == col) {
-                if(Grid[i][j] != 9)
-                    Grid[i][j] += 1;
-                
-            }
-                
+                if (seq == false) {
+                    if (Grid[i][j] != 9)
+                        Grid[i][j] += 1;
+                }
+                else
+                    if (Grid[i][j] != 0)
+                        Grid[i][j] -= 1;
+
+
+            }  
         }
     }
 }
@@ -147,11 +185,8 @@ int main()
 {   //Obj to main
     Board x1;
 
-    
     //Obj to the nested/inner class
     Board::Stack* x2 = new Board::Stack;
-
-    
 
    // int row, cow;
     char ans = 'y';
@@ -169,6 +204,8 @@ int main()
 
     std::cout << "From this...Ehh...Good Luck Then!\n";
 
+    x1.ShowBoard();
+
     /*
     do {
 
@@ -180,7 +217,9 @@ int main()
     */
     do {
        
-        x1.ShowBoard();
+        std::cout << std::endl;
+
+       // x1.ShowBoard();
         
         do {
             std::cout << "\n#Select the row and cow \n#In the range of 1-3\n";
@@ -189,10 +228,25 @@ int main()
 
         }while (x1.row < 1 || x1.row>3 || x1.col < 1 || x1.col>3);
         
-        x1.changeGrid(x1.row -1, x1.col -1);
+        x1.changeGrid(x1.row -1, x1.col -1,0);
+
+        x1.ShowBoard();
+
+
+        std::cout << std::endl;
+        //push
         
-        x2 -> next = new Board::Stack(x1.row,x1.col,nullptr);
         
+
+        x2->next = new Board::Stack(x1.row, x1.col, x2->next);
+        
+        
+        std::cout << "Undo?\n#"; 
+        std::cin >> ans;
+        if (ans == 'y')
+            x2->Undo(x1);
+        else
+            std::cout << "Oh\n";
 
     } while (x1.Finish()!=1);
 
@@ -200,7 +254,7 @@ int main()
     
 
     x1.ShowBoard();
-
+    delete x2;
 
     return 0;
 }
